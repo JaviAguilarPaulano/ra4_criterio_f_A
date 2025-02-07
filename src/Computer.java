d6f6fa13b4533840d9c51737c23bdd79bf55c04d
JaviAguilarPaulano <jagupau2110@g.educaand.es>
2025-02-07 14:23:25 +0100

    Creacion de setter

--- src/Computer.java
+++ src/Computer.java
@@ -7,4 +7,19 @@ public class Computer {
     private int ram;
     private float cpu;
     private String motherBoard;
+
+    // Setter para ram
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    // Setter para cpu
+    public void setCpu(float cpu) {
+        this.cpu = cpu;
+    }
+
+    // Setter para motherBoard
+    public void setMotherBoard(String motherBoard) {
+        this.motherBoard = motherBoard;
+    }
 }
